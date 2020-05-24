dbf18be85779d9356c2c97738d2f975d42498b31
trongio <giokakabadze50@gmail.com>
2020-05-24 15:59:07 +0400

    added register button

--- views/_layout.php
+++ views/_layout.php
@@ -17,7 +17,7 @@
 <body>
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container">
-        <a class="navbar-brand" href="#">Navbar</a>
+        <a class="navbar-brand" href="/">Navbar</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
                 aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
@@ -48,6 +48,10 @@
                         </a>
                     </li>
                 <?php else: ?>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/register">Register</a>
+                    </li>
+
                     <li class="nav-item">
                         <a class="nav-link" href="/login">Login</a>
                     </li>
